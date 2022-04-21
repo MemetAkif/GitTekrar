577ff4a9c72a633f05807a2dea12c5085ad5cd4c
Mehmet Akif Yuksel <67182475+MemetAkif@users.noreply.github.com>
2022-04-21 12:08:24 +0200

    uzaktan gelen

--- src/hamzahoca/IlkDers.java
+++ src/hamzahoca/IlkDers.java
@@ -5,6 +5,7 @@ public class IlkDers {
 	public static void main(String[] args) {
 		System.out.println("Saat 17:50");
 		System.out.println("Saat 13:28");
+		System.out.println("Saat 12:28 uzak");
 
 	}
 
