e98fcfd6ac708dd3eac6f73a465943a6df41baa6
Mehmet <haciarifyildiz@gmail.com>
2022-04-23 12:12:02 +0200

    cakisma

--- src/hamzahoca/IlkDers.java
+++ src/hamzahoca/IlkDers.java
@@ -7,7 +7,8 @@ public class IlkDers {
 		System.out.println("Saat 13:28");
 		System.out.println("Saat 12:28 uzak");
 		System.out.println("Saat 12:Bilgisayar");
-		System.out.println("Tekrar");
+		System.out.println("Tekrar cakisma kontrol");
+		// 2:35 de kaldik 
 	}
 
 }
